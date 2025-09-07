10e85606d57231f151d175621e92bbbd2bff11f1
CoolMinh <ngominh2312locthuan@gmail.com>
2025-09-07 13:30:26 +0000

    plz

--- main.py
+++ main.py
@@ -1,33 +1,26 @@
-import streamlit as  st
-appetizer=['Bánh mì nướng phomai','súp hành tây Pháp','Salad Caesar','Gỏi cuốn','Bánh mì bơ tỏi']
-main = ['Pizza','Pad Thai','Steak','Moussaka','Paella']
-dessert=['Cheesecake','Tiramisu','Crème brulée','Panna cotta','Trifle']
-with st.form('Thực đơn yêu thích'):
-  option1 = st.multiselect('Món khai vị ưu thích của bạn?',appetizer)
-  option2 = st.multiselect('Món chính ưu thích của bạn?',main)
-  option3 = st.multiselect('Món tráng miệng ưu thích của bạn?',dessert)
-  submitted = st.form_submit_button('Submit')
-if submitted:
-  st.write('các lựa chọn của bạn là:')
-  st.write('**1.món khai vị:**')
-  if len(option1)==0:
-    st.write('Bạn chưa chọn món khai vị')
-  else:
-    for i in range(len(option1)):
-      st.write(option1[i])
+import streamlit as st
 
-  st.write('**2.món chính:**')
-  if len(option2)==0:
-    st.write('Bạn chưa chọn món chính')
-  else:
-    for i in range(len(option2)):
-      st.write(option2[i])
+with st.form('Order đồ uống'):
+    drinks = ('Trà sữa truyền thống', 'Trà sữa matcha', 'Trà sữa trái cây')
+    option_drink = st.selectbox('Bạn muốn loại đồ uống gì?', drinks)
+    sugars = ('Đường trắng', 'Đường nâu', 'Không thêm đường')
+    option_sugar = st.selectbox('Bạn thích thêm loại đường nào cho đồ uống của bạn?', sugars)
+    jellys = ('Thạch rau câu', 'Thạch nha đam', 'Không thêm thạch')
+    option_jelly = st.selectbox('Bạn thích thêm loại thạch nào cho đồ uống của bạn?', jellys)
+    nums = st.slider('Số lượng bạn muốn đặt:', 0, 10, 0)
 
-  st.write('**3.món tráng miệng:**')
-  if len(option3)==0:
-    st.write('Bạn chưa chọn món tráng miệng')
-  else:
-    for i in range(len(option3)):
-      st.write(option3[i])
-      
+    bill = {'Loại đồ uống:': option_drink,'Loại đường:': option_sugar,'Loại thạch:': option_jelly,'Số lượng:': nums}
+    submitted = st.form_submit_button("Xác nhận")
+    if submitted:
+        st.write('Bạn đã chọn:')
+        for x, y in bill.items():
+            st.write(x, y)
+
+print_bill = st.checkbox('In hoá đơn')
+if print_bill:
+    ans = ''
+    for x in bill:
+        ans += str(x) + ' ' + str(bill[x]) + '\n'
+    st.download_button('In hoá đơn', ans)
+
       
